f8e280d1ba1d55d5268a0386df2ce2cf7eee601c
raymondstevens1881 <159415762+raymondstevens1881@users.noreply.github.com>
2026-03-13 13:17:38 -0400

    Update and rename ClamAV_GUI_V.1.2.1.py to ClamAV_GUI_V.1.2.1.cpp
    Signed-off-by: raymondstevens1881 <159415762+raymondstevens1881@users.noreply.github.com>

---
+++ ClamAV_GUI_V.1.2.1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+// Due to several states outsourcing parental responsibility by encouraging multiple states to impose age-verification on our systems, 
+I am removing the code to demonstrate in protest and opposition to these policies.//
+
+// So from this point forward, any updates to this project will come from any and all forked variations of this project, since the supporters of age-
+verification insist on mandating what should be their responsibility on those of us who detest and resist these laws, I am removing my contributions to any
+and all forms of open-source software until further notice. Don't like it? Join in protest and opposition to age-verification laws. And if you're supporting
+age verification laws? Well, here's your next update.//
+
+cout << "🖕" << endl;
