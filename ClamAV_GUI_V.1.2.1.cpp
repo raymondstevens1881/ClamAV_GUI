b0cfecaf1fd33ca0f69acea07fa83c3a8f572252
raymondstevens1881 <159415762+raymondstevens1881@users.noreply.github.com>
2026-03-15 17:54:05 -0400

    Update Notice.cpp
    Signed-off-by: raymondstevens1881 <159415762+raymondstevens1881@users.noreply.github.com>

--- ClamAV_GUI_V.1.2.1.cpp
+++ ClamAV_GUI_V.1.2.1.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
 
-// Due to several states outsourcing parental responsibility by encouraging multiple states to impose age-verification on our systems, 
-I am removing the code to demonstrate in protest and opposition to these policies.//
+// Due to several states outsourcing parental responsibility by encouraging multiple states to impose age-verification on our systems,
+// I am removing the code to demonstrate in protest and opposition to these policies.
 
-// So from this point forward, any updates to this project will come from any and all forked variations of this project, since the supporters of age-
-verification insist on mandating what should be their responsibility on those of us who detest and resist these laws, I am removing my contributions to any
-and all forms of open-source software until further notice. Don't like it? Join in protest and opposition to age-verification laws. And if you're supporting
-age verification laws? Well, here's your next update.//
+// From this point forward, any updates to this project will come from forked variations, 
+// since supporters of age-verification insist on mandating what should be their responsibility.
+// I am removing my contributions to any and all forms of open-source software until further notice.
+
+// Ethical systems are constructed to endure, grounded in principles that guide behavior consistently over time.
+// Moralist systems, by contrast, are like houses built on sand: they appear imposing and orderly, but their foundation is unstable.
+// When pressure comes—a tsunami of real-world complexity or hypocrisy—they collapse, leaving only the illusion of stability. 
+// Calling such fragile structures “prime real estate” is nothing more than a performative boast, 
+//one that obscures the true risk inherent in building on shaky moral pretenses.
+
+// So if anyone challenges me for not defending these laws, I’ll respond with this:
+// “They intend to air the dirty laundry, but have no intention of washing it.”
+// For those who are against age-verification laws, keep up the protests and defiant behaviors.
+// For those who support it, well, here's your next update.
 
 cout << "🖕" << endl;
